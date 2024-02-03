d1f9ce25442b81037b5a050fadd99972cc7c1b76
kzankpe <78028172+kzankpe@users.noreply.github.com>
2024-02-03 22:38:23 +0000

    feat: #8 create vnet resource

---
+++ src/variables.tf
@@ -0,0 +1,62 @@
+#####################################################
+# AZURE PROVIDER CONFIGURATION
+#####################################################
+
+variable "msi_endpoint" {
+  description = "Managned identity for deployment"
+}
+
+#####################################################
+# GENERAL VARIABLES
+#####################################################
+variable "location" {
+  description = "Azure region for resources. Default is West Europe"
+  default     = "westeurope"
+}
+
+variable "net_security" {
+  description = "Virtual Network Security Name"
+  type        = string
+  default     = "kzk8snsgne01"
+}
+
+variable "vnet_rg_name" {
+  description = "Virtual Network RG name"
+  type        = string
+  default     = "rgnetne01"
+}
+
+variable "vnet_name" {
+  description = "Virtual Network Name"
+  type        = string
+  default     = "kzk8svnetne01"
+}
+variable "vnet_address_space" {
+  description = "VNet addres space. Default is 10.10.0.0/16"
+  type        = list(string)
+  default     = ["10.10.0.0/16"]
+}
+
+variable "cluster_sub_address" {
+  description = "Cluster subnet"
+  type        = string
+  default     = "10.10.1.0/24"
+}
+
+variable "worker_sub_address" {
+  description = "Worker subnet"
+  type        = string
+  default     = "10.10.2.0/24"
+}
+
+variable "cluster_sub_name" {
+  description = "Cluster Subnet Name"
+  type        = string
+  default     = "kzclustersub"
+}
+
+variable "worker_sub_name" {
+  description = "Worker Subnet Name"
+  type        = string
+  default     = "kzclworkersub"
+}
